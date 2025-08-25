22d3b0574fb572e7bd811bc75fd6d167c76cfda8
Jongeun <jongey0121@gmail.com>
2025-08-25 14:47:17 +0900

    assignments_python

---
+++ assignments/set.py
@@ -0,0 +1,29 @@
+# 집합 과제 
+
+users = {
+    "Alice": ["music", "movies", "reading"],
+    "Bob": ["Sports", "trip", "music"],
+    "Charlie": ["programming", "game", "movies"],
+    "David": ["cooking", "trip", "photograph"],
+    "Eve": ["programming", "reading", "music"],
+    "Frank": ["sports", "game", "cooking"],
+    "Grace": ["movies", "trip", "reading"]
+}
+
+def common_interest(u1, u2):
+    set1 = set(users[u1])
+    set2 = set(users[u2])
+    return set1 & set2 
+
+
+def reply_common_interest(u1,u2):
+    common = common_interest(u1,u2)
+    if common: 
+        return f"{u1}와 {u2}의 공통 관심사는 {common} 입니다"
+    else:
+        return f"{u1}와 {u2}는 공통 관심사가 없습니다."
+    
+
+print(reply_common_interest("Eve","Charlie")) # 공통 관심사 1개 
+print(reply_common_interest("Alice","Grace")) # 공통 관심사 2개 
+print(reply_common_interest("Frank","Grace")) # 공통 관심사 없음 
